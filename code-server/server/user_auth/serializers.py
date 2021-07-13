9f0b42a8a000138f3a43fd05fd94047d837834df
LeoLin930104 <LeoLin930104@outlook.com>
2021-07-13 14:57:49 +0800

    minor changes

--- code-server/server/user_auth/serializers.py
+++ code-server/server/user_auth/serializers.py
@@ -2,39 +2,41 @@ from rest_framework import serializers
 
 from .models import User
 
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username']
+        fields = ["id", "username"]
+
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password2 = serializers.CharField(style={'input_type': 'password'}, required=True, write_only=True)
+    password2 = serializers.CharField(
+        style={"input_type": "password"}, required=True, write_only=True
+    )
 
     class Meta:
         model = User
-        fields = ['email', 'username', 'password', 'password2']
-        extra_kwargs = {
-            'password': { 'write_only': True }
-        }
+        fields = ["email", "username", "password", "password2"]
+        extra_kwargs = {"password": {"write_only": True}}
+
     def save(self):
         user = User(
-            email=self.validated_data['email'],
-            username=self.validated_data['username'],
+            email=self.validated_data["email"],
+            username=self.validated_data["username"],
         )
-        password = self.validated_data['password']
-        password2 = self.validated_data['password2']
+        password = self.validated_data["password"]
+        password2 = self.validated_data["password2"]
 
         if password != password2:
-            raise serializers.ValidationError({'password': 'Passwords much match.'})
+            raise serializers.ValidationError({"password": "Passwords much match."})
 
         user.set_password(password)
         user.save()
         return user
 
+
 class LoginSerializer(serializers.ModelSerializer):
-        class Meta:
-            model = User
-            fields = ['email', 'username', 'password']
-            extra_kwargs = {
-                'password': { 'write_only': True }
-            }
+    class Meta:
+        model = User
+        fields = ["email", "username", "password"]
+        extra_kwargs = {"password": {"write_only": True}}
