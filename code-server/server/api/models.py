b6da51b8a8b5ede3882da7c270a4858c89e6fa7c
LeoLin930104 <LeoLin930104@outlook.com>
2021-07-13 16:26:14 +0800

    time stamp for models

--- code-server/server/api/models.py
+++ code-server/server/api/models.py
@@ -3,15 +3,13 @@ from django.db import models
 # Create your models here.
 class CarbonEntry(models.Model):
     name = models.TextField(max_length=256)
-<<<<<<< HEAD
-    emission = models.IntegerField(null=False, default=0)
-=======
     emission = models.IntegerField(null=False)
->>>>>>> dabb72837f7c1215ed39e650f417de7c5772a1f5
     true_emission = models.IntegerField(null=False, default=0)
     tree_emission = models.IntegerField(null=False, default=0)
+    time = models.TimeField(auto_now=False, auto_now_add=True)
 
 
 class Donation(models.Model):
     username = models.TextField(max_length=256)
     amount = models.IntegerField(null=False)
+    time = models.TimeField(auto_now=False, auto_now_add=True)
