5fd858e4b26d033d93fab33009ef4d502987bba2
AaronWangTW <68408616+AaronWangTW@users.noreply.github.com>
2021-07-13 13:20:25 +0800

    username fix

--- code-client/src/components/signup.jsx
+++ code-client/src/components/signup.jsx
@@ -46,27 +46,16 @@ export default class SignUp extends React.Component {
         </Typography>
         <form className={classes.form} noValidate>
           <Grid container spacing={2}>
-            <Grid item xs={12} sm={6}>
+            <Grid item xs={12}>
               <TextField
-                autoComplete="fname"
-                name="firstName"
+                autoComplete="username"
+                name="userName"
                 variant="outlined"
                 required
                 fullWidth
-                id="firstName"
-                label="First Name"
+                id="userName"
+                label="User Name"
                 autoFocus
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                id="lastName"
-                label="Last Name"
-                name="lastName"
-                autoComplete="lname"
               />
             </Grid>
             <Grid item xs={12}>
