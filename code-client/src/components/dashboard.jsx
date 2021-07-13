f04978140cd31ad90ac31c10cddc8be05b20ecaf
AaronWangTW <68408616+AaronWangTW@users.noreply.github.com>
2021-07-13 20:35:29 +0800

    added donut chart

--- code-client/src/components/dashboard.jsx
+++ code-client/src/components/dashboard.jsx
@@ -6,8 +6,12 @@ import {
     Title,
     ArgumentAxis,
     ValueAxis,
+    PieSeries,
 } from '@devexpress/dx-react-chart-material-ui';
 import { Animation } from '@devexpress/dx-react-chart';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+import { makeStyles } from '@material-ui/core/styles';
 
 const barPlaceholderData = [
     { year: '1950', population: 2.525 },
@@ -18,13 +22,46 @@ const barPlaceholderData = [
     { year: '2000', population: 6.127 },
     { year: '2010', population: 6.930 },
 ];
+const docutData = [
+    { region: 'Asia', val: 4119626293 },
+    { region: 'Africa', val: 1012956064 },
+    { region: 'Northern America', val: 344124520 },
+    { region: 'Latin America and the Caribbean', val: 590946440 },
+    { region: 'Europe', val: 727082222 },
+    { region: 'Oceania', val: 35104756 },
+  ];
 
+const useStyles = makeStyles((theme) => ({
+    margin: {
+      margin: theme.spacing(1),
+    },
+    extendedIcon: {
+      marginRight: theme.spacing(1),
+    },
+  }));
+
+  const style = {
+    margin: 0,
+    top: 'auto',
+    right: 20,
+    bottom: 20,
+    left: 'auto',
+    position: 'fixed',
+};
+
+
+const classes = useStyles;
 export default class Dashboard extends React.Component {
+    
 
     constructor(props) {
         super(props);
         this.state = {
         };
+    }
+
+    getCarbonEmission(days){
+
     }
 
     render() {
@@ -44,6 +81,25 @@ export default class Dashboard extends React.Component {
                     <Animation />
                 </Chart>
             </Paper>
+            <Paper>
+        <Chart
+          data={docutData}
+        >
+          <PieSeries
+            valueField="val"
+            argumentField="region"
+            innerRadius={0.6}
+          />
+          <Title
+            text="The Population of Continents and Regions"
+          />
+          <Animation />
+        </Chart>
+      </Paper>
+            <Fab variant="extended" color="primary" aria-label="add" className={classes.margin} style={style}>
+                <AddIcon/>
+                Add Entry
+            </Fab>
         </div>
     }
 }
