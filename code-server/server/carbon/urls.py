f6683b4c3032d8555f8597a03415c80c2bf52d80
LeoLin930104 <LeoLin930104@outlook.com>
2021-07-14 16:18:04 +0800

    recentDataApi done, need test

--- code-server/server/carbon/urls.py
+++ code-server/server/carbon/urls.py
@@ -1,15 +1,23 @@
 from django.urls import path
 
-from . import views
+import views
 
-'''
+"""
 The Router for the Carbon App is written here 
-'''
+"""
 
 urlpatterns = [
-    path('logs', views.PersonalEntriesAPI.as_view(), name="logs"),
-    path('create', views.AddEntriesAPI.as_view(), name="create"),
-    path('recent-items', views.recentDataAPI.as_view({'get': 'list'}), name="recent-data"),
-    path('related-items', views.relatedItemsAPI.as_view({"get": "retrieve"}), name="related-items"),
-    path('create-item', views.postItemsAPI.as_view(), name="create-item"),
+    path("logs", views.PersonalEntriesAPI.as_view(), name="logs"),
+    path("create", views.AddEntriesAPI.as_view(), name="create"),
+    path(
+        "recent-entries/<int:days>/",
+        views.recentDataAPI.as_view({"get": "list"}),
+        name="recent-entries",
+    ),
+    path(
+        "related-items",
+        views.relatedItemsAPI.as_view({"get": "retrieve"}),
+        name="related-items",
+    ),
+    path("create-item", views.postItemsAPI.as_view(), name="create-item"),
 ]
