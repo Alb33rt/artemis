8bba9b51b401bf74b739a686e39a3e4d5a697daf
LeoLin930104 <LeoLin930104@outlook.com>
2021-07-14 10:27:58 +0800

    Changed models item with uit

--- code-server/server/carbon/models.py
+++ code-server/server/carbon/models.py
@@ -8,19 +8,25 @@ class Item(models.Model):
     name = models.TextField(max_length=256)
     emission = models.IntegerField(null=False)
     tree_emission = models.IntegerField(null=False, default=0)
+    unit = models.TextField(max_length=256)
     owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
+
 class CarbonEntry(models.Model):
-    item_involved = models.ForeignKey(Item, on_delete=models.CASCADE, related_name="CarbonEntry")
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="CarbonEntry")
+    item_involved = models.ForeignKey(
+        Item, on_delete=models.CASCADE, related_name="CarbonEntry"
+    )
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="CarbonEntry"
+    )
     time_created = models.DateTimeField(auto_now_add=True)
     quantity = models.FloatField(default=1)
     details = models.TextField(max_length=256)
 
     def get_emissions(self):
-        total = self.item_involved['emission'] * self.quantity
+        total = self.item_involved["emission"] * self.quantity
         return total
 
     def get_trees(self):
-        trees = self.item_involved['tree_emission'] * self.quantity
+        trees = self.item_involved["tree_emission"] * self.quantity
         return trees
