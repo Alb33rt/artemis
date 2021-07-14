45e23157939c56fc15f387dbbdbf233e40aea0d0
Matt940624 <71871298+Matt940624@users.noreply.github.com>
2021-07-15 07:03:15 +0800

    I am at the end of my wits (help me)

--- code-client/src/components/contactus.jsx
+++ code-client/src/components/contactus.jsx
@@ -1,10 +1,122 @@
-import React from "react";
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import CameraIcon from '@material-ui/icons/PhotoCamera';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Link from '@material-ui/core/Link';
 
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
+const useStyles = makeStyles((theme) => ({
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4),
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  cardMedia: {
+    paddingTop: '56.25%', // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(6),
+  },
+}));
 
-export default class ContactUs extends React.Component {
-    
-    render(){
-        return <p>Contact Us</p>
-    }
+const cards = [1];
+
+export default function Album() {
+  const classes = useStyles();
+
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      
+      <main>
+        {/* Hero unit */}
+        <div className={classes.heroContent}>
+          <Container maxWidth="sm">
+            <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+              Contact Us
+            </Typography>
+            <Typography variant="h5" align="center" color="textSecondary" paragraph>
+                Get in touch with us if you have any questions
+            </Typography>
+          
+          </Container>
+        </div>
+        <Grid Container >
+            <Grid item xs={3}></Grid>
+            <Grid item xs={3}>
+                <Card style={{display:'blocks', justifyContent:'center'}}>
+                    <Typography gutterBottom variant="h5" component="h2"> Member: Albert</Typography>
+                    <n></n>
+                    <Typography gutterBottom variant="h5" component="h2"> Email: Albert@gmail.com</Typography>
+                </Card>
+            </Grid>
+            <Grid item xs={3}>
+                <Card style={{display:'blocks', justifyContent:'center'}}>
+                    <Typography gutterBottom variant="h5" component="h2"> Member: Aaron</Typography>
+                    <n></n>
+                    <Typography gutterBottom variant="h5" component="h2"> Email: Aaron@gmail.com</Typography>
+                </Card>
+            </Grid>
+            <Grid item xs={3}></Grid>
+            <Grid item xs={3}></Grid>
+            <Grid item xs={3}>
+                <Card style={{display:'blocks', justifyContent:'center'}}>
+                    <Typography gutterBottom variant="h5" component="h2"> Member: Leo</Typography>
+                    <n></n>
+                    <Typography gutterBottom variant="h5" component="h2"> Email: Leo@gmail.com</Typography>
+                </Card>
+            </Grid>
+            <Grid item xs={3}>
+                <Card style={{display:'blocks', justifyContent:'center'}}>
+                    <Typography gutterBottom variant="h5" component="h2"> Member: Matt</Typography>
+                    <n></n>
+                    <Typography gutterBottom variant="h5" component="h2"> Email: Matt@gmail.com</Typography>
+                </Card>
+            </Grid>
+            <Grid item xs={3}></Grid>
+        </Grid>
+      </main>
+
+    </React.Fragment>
+  );
 }
