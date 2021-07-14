a92185ece0c259258c57eb6b08d361c86770ff45
LeoLin930104 <LeoLin930104@outlook.com>
2021-07-15 04:09:35 +0800

    add server admin register for green item and entry

--- code-server/server/carbon/admin.py
+++ code-server/server/carbon/admin.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
 
-from .models import CarbonItem, CarbonEntry
+from .models import CarbonItem, CarbonEntry, GreenItem, GreenEntry
 
 
 class CarbonItemAdmin(admin.ModelAdmin):
-    fields = ('name', 'emission', 'owner', 'unit')
+    fields = ("name", "emission", "owner", "unit")
+
+
+class CarbonItemAdmin(admin.ModelAdmin):
+    fields = ("name", "oxygen", "owner", "unit")
 
 
 # Register your models here.
 admin.site.register(CarbonItem, CarbonItemAdmin)
 admin.site.register(CarbonEntry)
+admin.site.register(GreenItem, GreenItemAdmin)
+admin.site.register(GreenEntry)
