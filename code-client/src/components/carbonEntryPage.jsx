f9fb7f032440eb3aa280d18601c272cd4e7422f4
Matt940624 <71871298+Matt940624@users.noreply.github.com>
2021-07-14 14:14:07 +0800

    added all text into home (need images and refining)

--- code-client/src/components/carbonEntryPage.jsx
+++ code-client/src/components/carbonEntryPage.jsx
@@ -1,4 +1,15 @@
 import React from "react";
+import Container from '@material-ui/core/Container';
+import styled, { css } from 'styled-components/macro'
+import backgroundImage from '../images/545792.jpg';
+import { withRouter } from "react-router";
+import PropTypes from 'prop-types';
+import { makeStyles, ThemeProvider, withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Link from '@material-ui/core/Link';
+import { theme } from "../colorTheme";
 
 
 export default class CarbonEntryPage extends React.Component{
@@ -8,3 +19,4 @@ export default class CarbonEntryPage extends React.Component{
         return <p>Carbon Entry Page</p>
     }
 }
+
